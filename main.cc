675a4106946592f43d3a1d447d9ea6922857a56a
Gia Reyes <giareyes2005@gmail.com>
2022-05-02 10:55:00 -0400

    add sphere and hittable classes

--- main.cc
+++ main.cc
@@ -2,31 +2,36 @@
 #include "vec3.h"
 #include "ray.h"
 #include "color.h"
+#include "sphere.h"
 
 // compile with g++ -std=c++11 main.cc -o main because for some reason the default g++ on my computer
 // uses version 4.2.1 ??? TToTT
-double hit_sphere( const point3& center, double radius, const ray& r ) {
-  // r^2 = (t^2 * b . b ) + 2tb ( A - C ) + (a - c )^2
-  vec3 oc = r.origin() - center;
-  double a = r.direction().length_squared();
-  double hb = dot( r.direction(), oc );
-  double c = oc.length_squared() - radius*radius;
-
-  double discriminant = hb*hb - a*c;
-  if( discriminant < 0 ) {
-    return -1.0;
-  }
-  else {
-    return ( -hb - sqrt( discriminant ) ) / a;
-  }
-}
+// double hit_sphere( const point3& center, double radius, const ray& r ) {
+//   // r^2 = (t^2 * b . b ) + 2tb ( A - C ) + (a - c )^2
+//   vec3 oc = r.origin() - center;
+//   double a = r.direction().length_squared();
+//   double hb = dot( r.direction(), oc );
+//   double c = oc.length_squared() - radius*radius;
+//
+//   double discriminant = hb*hb - a*c;
+//   if( discriminant < 0 ) {
+//     return -1.0;
+//   }
+//   else {
+//     return ( -hb - sqrt( discriminant ) ) / a;
+//   }
+// }
 
 color ray_color( const ray& r ) {
-  double contact_pt = hit_sphere( point3( 0, 0, -1 ), 0.5, r );
-  if( contact_pt > 0.0 ) {
-    vec3 normal = unit_vector( r.at( contact_pt ) - point3( 0, 0, -1 ) ); // hit point - center of sphere
-    return 0.5*color( normal.x() + 1.0, normal.y() + 1, normal.z() + 1 );
-  }
+  sphere s1 = sphere( point3( 0, 0, -1 ), 0.5 );
+  hit_record rec;
+  if( s1.hit( r, 0, std::numeric_limits<double>::infinity(), rec ) )
+    return 0.5*( rec.normal + color( 1.0, 1.0, 1.0 ) );
+  // double contact_pt = hit_sphere( point3( 0, 0, -1 ), 0.5, r );
+  // if( contact_pt > 0.0 ) {
+  //   vec3 normal = unit_vector( r.at( contact_pt ) - point3( 0, 0, -1 ) ); // hit point - center of sphere
+  //   return 0.5*color( normal.x() + 1.0, normal.y() + 1, normal.z() + 1 );
+  // }
   vec3 unit_direction = unit_vector( r.direction() );
   auto t = 0.5*( unit_direction.y() + 1 );
   return ( 1.0 - t )*color( 1.0, 1.0, 1.0 ) + t*color( 0.5, 0.7, 1.0 );
---
+++ sphere.h
@@ -0,0 +1,41 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+  public:
+    sphere() {}
+    sphere( point3 cen, double r ): center( cen ), radius ( r ) {}
+
+    virtual bool hit( const ray& r, double t_min, double t_max, hit_record& rec ) const override;
+
+  public:
+    point3 center;
+    double radius;
+};
+
+bool sphere::hit( const ray& r, double t_min, double t_max, hit_record& rec ) const {
+  vec3 oc = r.origin() - center;
+  double a = r.direction().length_squared();
+  double hb = dot( r.direction(), oc );
+  double c = oc.length_squared() - radius*radius;
+
+  double discriminant = hb*hb - a*c;
+  if( discriminant < 0 ) return false;
+
+  auto root = ( -hb - sqrt( discriminant ) ) / a;
+  if( root < t_min || root > t_max ) {
+    root = ( -hb + sqrt( discriminant ) ) / a;
+    if( root < t_min || root > t_max ) return false;
+  };
+
+  rec.t = root;
+  rec.p = r.at( root );
+  rec.normal = ( r.at( root ) - center )/ radius;
+
+  return true;
+};
+
+#endif
